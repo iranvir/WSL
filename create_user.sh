b8bf7a39d9fe8fbaab1b59b258cbd5aa40fb2ac1
Ranvir Singh <sranvir155@gmail.com>
2021-08-08 09:41:39 +0530

    Remove Create User, add compiled kernel

--- create_user.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-useradd -m -s /bin/bash ranvir
-passwd -d ranvir
-echo "[user]" >> /etc/wsl.conf
-echo "default=ranvir" >> /etc/wsl.conf
-echo "echo \"ranvir ALL=(ALL) NOPASSWD:ALL\" >> /etc/sudoers" | sudo bash
